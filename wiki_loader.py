508351c3a4bb14770329c7ebf30b34f910ae32cc
skdev <skdev@ucsc.edu>
2025-04-13 16:56:16 -0700

    Flashcards with search term: Needs work to not truncate after 16000 tokens

--- wiki_loader.py
+++ wiki_loader.py
@@ -3,6 +3,3 @@ import wikipedia
 def parse(article_name):
     page = wikipedia.page(article_name, auto_suggest = False)
     return page.content
-    
-if __name__ == "__main__":
-    print(parse("Karl Marx"))
